326f4cbd199f33d94d73731f056130a11a328a89
waxsh88 <wlpjxn@qq.com>
2021-10-24 02:14:29 +0800

    Create docker-entrypoint.sh

---
+++ docker-entrypoint.sh
@@ -0,0 +1,55 @@
+#!/bin/sh
+
+CODE_DIR=/xdd-plus
+
+if [  "$ENABLE_GOPROXY" = "true" ]; then
+  export GOPROXY=https://goproxy.io,direct 
+  echo "启用 goproxy 加速 ${GOPROXY}"
+else
+  echo "未启用 goproxy 加速"
+fi
+
+if [ "$ENABLE_GITHUBPROXY" = "true" ]; then
+   GITHUBPROXY=https://ghproxy.com/
+   echo "启用 github 加速 ${GITHUBPROXY}"
+else
+  echo "未启用 github 加速"
+fi
+
+if [ "$ENABLE_APKPROXY" = "true" ]; then
+  sed -i 's/dl-cdn.alpinelinux.org/mirrors.aliyun.com/g' /etc/apk/repositories
+  echo "启用 alpine APK 加速 mirrors.aliyun.com"
+else
+  sed -i 's/mirrors.aliyun.com/dl-cdn.alpinelinux.org/g' /etc/apk/repositories
+  echo "未启用 alpine APK 加速"
+fi
+
+if [ -z $REPO_URL ]; then
+  REPO_URL=${GITHUBPROXY}https://github.com/764763903a/xdd-plus.git
+fi
+
+
+if ! type git  >/dev/null 2>&1; then
+  echo "正在安装git..."
+  apk add git
+else 
+  echo "git已安装"
+fi
+
+
+if [ ! -d $CODE_DIR/.git ]; then
+  echo "xdd-plus 核心代码目录为空, 开始clone代码..."
+  git clone $REPO_URL  $CODE_DIR
+else 
+  echo "xdd-plus 核心代码已存在"
+  echo "更新 xdd-plus 核心代码"
+  cd $CODE_DIR && git reset --hard && git pull
+fi
+
+
+echo "开始编译..."
+cd $CODE_DIR && go build
+
+
+echo "启动..."
+ ./xdd-plus
